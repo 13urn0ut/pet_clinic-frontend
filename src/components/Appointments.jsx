006b2e53c1baf0454c89766a107ebdf90f4e0cf0
D1JS <arturas.mickevicius@stud.techin.lt>
2025-02-14 09:43:32 +0400

    Add sort and filter dropdowns

--- src/components/Appointments.jsx
+++ src/components/Appointments.jsx
@@ -9,13 +9,40 @@ const API_URL = import.meta.env.VITE_API_URL;
 const Appointments = () => {
   const { appointments, setAppointments } = useContext(AppointmentContext);
   const [addAppointment, setAddAppointment] = useState(false);
+  const [filter, setFilter] = useState({
+    page: 1,
+    limit: 10,
+    sortBy: "",
+    confirmed: "",
+  });
+
+  const changeSortBY = (e) => {
+    setFilter({ ...filter, sortBy: e.target.value });
+  };
+
+  const changeConfirmed = (e) => {
+    setFilter({ ...filter, confirmed: e.target.value });
+  };
+
+  const queryStr =
+    "?" +
+    Object.entries(filter)
+      // eslint-disable-next-line no-unused-vars
+      .filter(([key, value]) => value)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&");
+
+  console.log(queryStr);
 
   useEffect(() => {
     const fetchAppointments = async () => {
       try {
-        const { data: result } = await axios.get(`${API_URL}/appointments`, {
-          withCredentials: true,
-        });
+        const { data: result } = await axios.get(
+          `${API_URL}/appointments${queryStr}`,
+          {
+            withCredentials: true,
+          }
+        );
 
         setAppointments(result.data);
       } catch (err) {
@@ -25,7 +52,7 @@ const Appointments = () => {
 
     fetchAppointments();
     setAddAppointment(false);
-  }, []);
+  }, [filter]);
 
   return (
     <div className="appointments-container w-max mx-auto">
@@ -36,6 +63,20 @@ const Appointments = () => {
       >
         Add Appointment
       </button>
+      <div>
+        <select name="sortBy" id="sortBy" onChange={changeSortBY}>
+          <option value="">Sort By</option>
+          <option value="date">Date</option>
+          <option value="confirmed">Confirmed</option>
+          <option value="rating">Rating</option>
+        </select>
+
+        <select name="confirmed" id="confirmed" onChange={changeConfirmed}>
+          <option value="">Filter By</option>
+          <option value="true">Confirmed</option>
+          <option value="false">Unconfirmed</option>
+        </select>
+      </div>
       {addAppointment && <AppointmentForm />}
       <div className="appointments-list flex flex-col gap-5 w-max mx-auto mt-4">
         {appointments.map((appointment) => (
